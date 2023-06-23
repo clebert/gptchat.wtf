f7d5841ea760bbb9b8e9f75e082e4c6f09598487
Clemens Akens <clebert@me.com>
2023-06-23 17:00:13 +0200

    Use `@heroicons/react` instead if hardcoded SVG data for icons

--- src/components/icon.tsx
+++ src/components/icon.tsx
@@ -1,4 +1,21 @@
 import {joinClassNames} from '../utils/join-class-names.js';
+import {
+  AdjustmentsHorizontalIcon,
+  ArrowRightOnRectangleIcon,
+  ArrowUturnLeftIcon,
+  ChatBubbleLeftEllipsisIcon,
+  ComputerDesktopIcon,
+  EyeIcon,
+  EyeSlashIcon,
+  KeyIcon,
+  MoonIcon,
+  PaperAirplaneIcon,
+  PlusIcon,
+  SunIcon,
+  TrashIcon,
+  UserIcon,
+  XMarkIcon,
+} from '@heroicons/react/24/outline';
 import * as React from 'react';
 
 export interface IconProps {
@@ -6,63 +23,30 @@ export interface IconProps {
   standalone?: boolean;
 }
 
+const iconHeightWidth = `h-4 w-4`;
+
 const pathByType = {
-  adjustmentsHorizontal: [
-    `M10.5 6h9.75M10.5 6a1.5 1.5 0 11-3 0m3 0a1.5 1.5 0 10-3 0M3.75 6H7.5m3 12h9.75m-9.75 0a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m-3.75 0H7.5m9-6h3.75m-3.75 0a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m-9.75 0h9.75`,
-  ],
-  arrowRightOnRectangle: [
-    `M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15m3 0l3-3m0 0l-3-3m3 3H9`,
-  ],
-  arrowUturnLeft: [`M9 15L3 9m0 0l6-6M3 9h12a6 6 0 010 12h-3`],
-  chatBubbleLeftEllipsis: [
-    `M8.625 9.75a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0H8.25m4.125 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0H12m4.125 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0h-.375m-13.5 3.01c0 1.6 1.123 2.994 2.707 3.227 1.087.16 2.185.283 3.293.369V21l4.184-4.183a1.14 1.14 0 01.778-.332 48.294 48.294 0 005.83-.498c1.585-.233 2.708-1.626 2.708-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0012 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018z`,
-  ],
-  computerDesktop: [
-    `M9 17.25v1.007a3 3 0 01-.879 2.122L7.5 21h9l-.621-.621A3 3 0 0115 18.257V17.25m6-12V15a2.25 2.25 0 01-2.25 2.25H5.25A2.25 2.25 0 013 15V5.25m18 0A2.25 2.25 0 0018.75 3H5.25A2.25 2.25 0 003 5.25m18 0V12a2.25 2.25 0 01-2.25 2.25H5.25A2.25 2.25 0 013 12V5.25`,
-  ],
-  eye: [
-    `M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z`,
-    `M15 12a3 3 0 11-6 0 3 3 0 016 0z`,
-  ],
-  eyeSlash: [
-    `M3.98 8.223A10.477 10.477 0 001.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.45 10.45 0 0112 4.5c4.756 0 8.773 3.162 10.065 7.498a10.523 10.523 0 01-4.293 5.774M6.228 6.228L3 3m3.228 3.228l3.65 3.65m7.894 7.894L21 21m-3.228-3.228l-3.65-3.65m0 0a3 3 0 10-4.243-4.243m4.242 4.242L9.88 9.88`,
-  ],
-  key: [
-    `M15.75 5.25a3 3 0 013 3m3 0a6 6 0 01-7.029 5.912c-.563-.097-1.159.026-1.563.43L10.5 17.25H8.25v2.25H6v2.25H2.25v-2.818c0-.597.237-1.17.659-1.591l6.499-6.499c.404-.404.527-1 .43-1.563A6 6 0 1121.75 8.25z`,
-  ],
-  moon: [
-    `M21.752 15.002A9.718 9.718 0 0118 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 003 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 009.002-5.998z`,
-  ],
-  paperAirplane: [
-    `M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5`,
-  ],
-  plus: [`M12 4.5v15m7.5-7.5h-15`],
-  sun: [
-    `M12 3v2.25m6.364.386l-1.591 1.591M21 12h-2.25m-.386 6.364l-1.591-1.591M12 18.75V21m-4.773-4.227l-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z`,
-  ],
-  trash: [
-    `M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0`,
-  ],
-  user: [
-    `M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z`,
-  ],
-  xMark: [`M6 18L18 6M6 6l12 12`],
+  adjustmentsHorizontal: <AdjustmentsHorizontalIcon className={iconHeightWidth} />,
+  arrowRightOnRectangle: <ArrowRightOnRectangleIcon className={iconHeightWidth} />,
+  arrowUturnLeft: <ArrowUturnLeftIcon className={iconHeightWidth} />,
+  chatBubbleLeftEllipsis: <ChatBubbleLeftEllipsisIcon className={iconHeightWidth} />,
+  computerDesktop: <ComputerDesktopIcon className={iconHeightWidth} />,
+  eye: <EyeIcon className={iconHeightWidth} />,
+  eyeSlash: <EyeSlashIcon className={iconHeightWidth} />,
+  key: <KeyIcon className={iconHeightWidth} />,
+  moon: <MoonIcon className={iconHeightWidth} />,
+  paperAirplane: <PaperAirplaneIcon className={iconHeightWidth} />,
+  plus: <PlusIcon className={iconHeightWidth} />,
+  sun: <SunIcon className={iconHeightWidth} />,
+  trash: <TrashIcon className={iconHeightWidth} />,
+  user: <UserIcon className={iconHeightWidth} />,
+  xMark: <XMarkIcon className={iconHeightWidth} />,
 } as const;
 
 export function Icon({type, standalone}: IconProps): JSX.Element {
   return (
     <div className={joinClassNames(`inline-flex h-5 align-middle`, !standalone && `mr-1`)}>
-      <svg
-        className="h-4 w-4 stroke-current stroke-1"
-        viewBox="0 0 24 24"
-        fill="none"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      >
-        {pathByType[type].map((d, index) => (
-          <path key={index} d={d} />
-        ))}
-      </svg>
+      {pathByType[type]}
     </div>
   );
 }
